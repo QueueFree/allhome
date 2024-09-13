74f9b1de953577d10f6d85ca44401e4cc8b8a631
QueueFree <kainow123asd@gmail.com>
2024-09-13 23:03:33 +0600

    c

--- handlers/buttons.py
+++ handlers/buttons.py
@@ -26,3 +26,8 @@ start_test = ReplyKeyboardMarkup(
     KeyboardButton('/dice'),
     KeyboardButton('/games')
 )
+
+cancel_button = ReplyKeyboardMarkup(resize_keyboard=True).add(KeyboardButton('Отмена'))
+
+submit_button = ReplyKeyboardMarkup(resize_keyboard=True,
+                                    row_width=2).add(KeyboardButton('Да'), KeyboardButton('Нет'))
--- handlers/dz1.py
+++ handlers/dz1.py
@@ -2,7 +2,7 @@ import logging
 from aiogram import types
 from aiogram.utils import executor
 from config import bot, admin, dp
-import commands, echo, quiz
+import commands, echo, quiz, fsm_reg, storage
 
 async def on_startup(_):
     for i in admin:
@@ -10,7 +10,8 @@ async def on_startup(_):
 
 commands.register_commands(dp)
 quiz.register_quiz(dp)
-
+fsm_reg.register_fsm_reg(dp)
+storage.register_store(dp)
 
 echo.register_echo(dp)
 
